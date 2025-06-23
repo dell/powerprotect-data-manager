69bd627abc2c43a001563a7c62d2ae438332a6de
IdanKen <59053005+IdanKen@users.noreply.github.com>
2025-06-23 21:51:22 +0300

    Added features and improvements
    -> Added a feature to accept cert by cert host
    -> Enhanced readability and performance

--- Python/certmgmt.py
+++ Python/certmgmt.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import requests
 import urllib3
-import sys
-import json
+
+# This script simplifies certificate management in PowerProtect DAta Manager
+# Author - Idan Kentor <idan.kentor@dell.com>
+# Copyright [2025] [Idan Kentor]
 
 # Script to simplify certificate management in PowerProtect Data Manager
 # Examples:
 # python certmgmt.py -s 10.0.0.1 -usr admin -pwd "myPassword!" -a list
 # python certmgmt.py -s 10.0.0.1 -usr admin -pwd "myPassword!" -a accept -id MTAuMjQ3LjUuNDU6MzAwOTpo4n8G
+# python certmgmt.py -s 10.0.0.1 -usr admin -pwd "myPassword!" -a accept -host api.idan.openshift-example.com
 
 urllib3.disable_warnings()
 
+
 def get_args():
-    # Get command line args from the user
+    """Get command line args from the user"""
     parser = argparse.ArgumentParser(
         description='Script to simplify certificate management in PowerProtect Data Manager')
     parser.add_argument('-s', '--server', required=True,
@@ -25,104 +30,138 @@ def get_args():
                         help='Password')
     parser.add_argument('-a', '--action', required=True, choices=['list', 'accept'],
                         help='Choose to list certificates or accept a specific one')
-    parser.add_argument('-id', '--id', required='accept' in sys.argv, action='store', default=None,
+    parser.add_argument('-id', '--id', required=False, action='store', default=None,
                         help='Optionally provide the ID the certificate to accept')
+    parser.add_argument('-cert-host', '--certhost', required=False, action='store', default=None,
+                        help='Optionally provide the certificate host to accept')
     args = parser.parse_args()
     return args
 
-def authenticate(ppdm, user, password, uri):
-    # Login
-    suffixurl = "/login"
-    uri += suffixurl
-    headers = {'Content-Type': 'application/json'}
-    payload = '{"username": "%s", "password": "%s"}' % (user, password)
+
+def init_rest_call(verb, uri, token, payload=None, params=None):
+    """Generic function for REST calls"""
+    if uri.endswith("/login"):
+        headers = {"Content-Type": "application/json"}
+    else:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " f"{token}",
+        }
+    payload = json.dumps(payload)
+    verify = False
+    timeout = 90
     try:
-        response = requests.post(uri, data=payload, headers=headers, verify=False)
+        if verb.lower() == "get":
+            response = requests.get(
+                uri,
+                headers=headers,
+                params=params,
+                verify=verify,
+                timeout=timeout
+            )
+        else:
+            response = requests.request(
+                verb,
+                uri,
+                headers=headers,
+                params=params,
+                data=payload,
+                verify=verify,
+                timeout=timeout,
+            )
         response.raise_for_status()
-    except requests.exceptions.ConnectionError as err:
-        print('Error Connecting to {}: {}'.format(ppdm, err))
-        sys.exit(1)
-    except requests.exceptions.Timeout as err:
-        print('Connection timed out {}: {}'.format(ppdm, err))
-        sys.exit(1)
-    except requests.exceptions.RequestException as err:
-        print("The call {} {} failed with exception:{}".format(response.request.method, response.url, err))
-        sys.exit(1)
-    if (response.status_code != 200):
-        raise Exception('Login failed for user: {}, code: {}, body: {}'.format(
-            user, response.status_code, response.text))
-    print('Login for user: {} to PPDM: {}'.format(user, ppdm))
-    token = response.json()['access_token']
+    except requests.exceptions.ConnectionError as error:
+        print(f"-> Error Connecting to {uri}: {error}")
+        raise SystemExit(1) from error
+    except requests.exceptions.Timeout as error:
+        print(f"-> Connection timed out {urllib3}: {error}")
+        raise SystemExit(1) from error
+    except requests.exceptions.RequestException as error:
+        if response.status_code in (401, 502):
+            return False
+        print(
+            f"-> The call {response.request.method} {response.url} \
+                failed with exception:{error}"
+        )
+    if not response.content:
+        return True
+    if uri.endswith("/login"):
+        return response.json()["access_token"]
+    try:
+        return response.json()
+    except (AttributeError, ValueError):
+        return response.content
+
+
+def authenticate(uri, user, password):
+    """Logins into PowerProtect Data Manager"""
+    uri = f"{uri}/login"
+    payload = {"username": user, "password": password}
+    token = init_rest_call("POST", uri, payload, payload)
     return token
 
-def get_certs(uri, token, id=None):
-    # Retrieves a list of credentials
-    suffixurl = "/certificates"
-    if id:
-        suffixurl = "/certificates/{}".format(id)
-    uri += suffixurl
-    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(token)}
-    try:
-        response = requests.get(uri, headers=headers, verify=False)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as err:
-        print("The call {}{} failed with exception:{}".format(response.request.method, response.url, err))
-    if (response.status_code != 200):
-        raise Exception('Failed to query {}, code: {}, body: {}'.format(
-				uri, response.status_code, response.text))
-    return response.json()
+
+def get_certs(uri, token, cert_id=None, cert_host=None):
+    """Retrieves a list of credentials"""
+    uri = f"{uri}/certificates"
+    query_params = None
+    if cert_id:
+        uri = f"{uri}/{cert_id}"
+    if cert_host:
+        query = f'host eq "{cert_host}"'
+        query_params = {'filter': query}
+    response = init_rest_call("GET", uri, token, None, query_params)
+    return response
+
 
 def accept_cert(uri, token, certs):
-    # Performs removal of credentials by ID
-    suffixurl = "/certificates/{}".format(certs["id"])
-    uri += suffixurl
-    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(token)}
-    try:
-        response = requests.post(uri, headers=headers, data=certs, verify=False)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as err:
-        print("The call {} {} failed with exception:{}".format(response.request.method, response.url, err))
-    if response.status_code not in [200, 201, 202, 204]:
-        print('Failed to accept certificate with ID {}, code: {}, body: {}'.format(
-				id, response.status_code, response.text))
-        return False
-    return True
+    """Performs removal of credentials by ID"""
+    uri = f"{uri}/certificates/{certs["id"]}"
+    response = init_rest_call("PUT", uri, token, payload=certs)
+    return response
 
-def logout(ppdm, user, uri, token):
-    # Logs out of PowerProtect
-    suffixurl = "/logout"
-    uri += suffixurl
-    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(token)}
-    try:
-        response = requests.post(uri, headers=headers, verify=False)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as err:
-            print("The call {} {} failed with exception:{}".format(response.request.method, response.url, err))
-    if (response.status_code != 204):
-        raise Exception('Logout failed for user: {}, code: {}, body: {}'.format(
-            user, response.status_code, response.text))
-    print('Logout for user: {} from PPDM: {}'.format(user, ppdm))
 
 def main():
     port = "8443"
     apiendpoint = "/api/v2"
     args = get_args()
-    ppdm, user, password, action, id = args.server, args.user, args.password, args.action, args.id
-    uri = "https://{}:{}{}".format(ppdm, port, apiendpoint)
-    token = authenticate(ppdm, user, password, uri)
-    if (action == 'list'):
+    ppdm, user, password = args.server, args.user, args.password
+    action, cert_id, cert_host = args.action, args.id, args.certhost
+    uri = f"https://{ppdm}:{port}{apiendpoint}"
+    token = authenticate(uri, user, password)
+    if action == "list":
         certs = get_certs(uri, token)
-        print(json.dumps(certs,indent=4))
-    elif (action == 'accept'):
-            print("Accepting certificate with ID: {}".format(id))
-            certs = get_certs(uri, token, id)
-            certs["state"] = "ACCEPTED"
-            result = accept_cert(uri, token, certs)
-            if result:
-                print("Certificate ID {} of host {} accepted successfully".format(certs["id"], certs["host"]))
-            else:
-                print("Certificate ID {} could not be accepted".format(certs["id"]))
-    logout(ppdm, user, uri, token)
+        print(json.dumps(certs, indent=4))
+    elif action == "accept":
+        certs = None
+        if not cert_host and not cert_id:
+            print("Specify either certificate host or ID. Exiting...")
+            raise SystemExit(1)
+        if cert_host:
+            if not cert_id:
+                certs = get_certs(uri, token, None, cert_host)
+                if not certs:
+                    print(f"Could not obtain certificate ID by host {cert_host}. Exiting...")
+                    raise SystemExit(1)
+                if "content" in certs:
+                    if len(certs["content"]) > 1:
+                        print(f"Certificate host {cert_host} yielded in more than 1 result. Exiting...")
+                        raise SystemExit(1)
+                    if len(certs["content"]) == 0:
+                        print(f"Certificate host {cert_host} Could not be found. Exiting...")
+                        raise SystemExit(1)
+                    certs = certs["content"][0]
+                cert_id = certs["id"]
+        if cert_id:
+            if not certs:
+                certs = get_certs(uri, token, cert_id)
+        certs["state"] = "ACCEPTED"
+        result = accept_cert(uri, token, certs)
+        if result:
+            print(f"Certificate ID {certs["id"]} of host {certs["host"]} accepted successfully")
+        else:
+            print(f"Certificate ID {certs["id"]} of host {certs["host"]} could not be accepted")
+
 
 if __name__ == "__main__":
     main()
